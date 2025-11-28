496c7012c30aa8b33b83ec55c1a6cd7fad66098a
BANK WALLET <usman2006dabzy@gmail.com>
2025-11-29 00:59:15 +0100

    Update page.tsx

--- app/buy-pay-id/payment/page.tsx
+++ app/buy-pay-id/payment/page.tsx
@@ -89,7 +89,7 @@ export default function PaymentPage() {
             <div>
               <p className="text-gray-700 mb-1">Account Number</p>
               <div className="flex items-center justify-between">
-                <p className="font-bold">1535694254</p>
+                <p className="font-bold">1451411508</p>
                 <button
                   onClick={handleCopyAccountNumber}
                   className="bg-orange-400 text-white px-3 py-1 rounded text-sm"
@@ -101,12 +101,12 @@ export default function PaymentPage() {
 
             <div>
               <p className="text-gray-700 mb-1">Bank Name</p>
-              <p className="font-bold">Nombank Microfinance Bank</p>
+              <p className="font-bold">Nombank Mfb</p>
             </div>
 
             <div>
               <p className="text-gray-700 mb-1">Account Name</p>
-              <p className="font-bold">Aermeans/Richard David</p>
+              <p className="font-bold">Usman Usman</p>
             </div>
           </div>
 
